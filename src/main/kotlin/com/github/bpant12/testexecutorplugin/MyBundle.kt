f24326d6bcd91d933c40f6e78f6b85cc13bf9c55
GitHub Action <action@github.com>
2022-12-29 09:12:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/bpant12/testexecutorplugin/MyBundle.kt
+++ src/main/kotlin/com/github/bpant12/testexecutorplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.bpant12.testexecutorplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/bpant12/testexecutorplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/bpant12/testexecutorplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.bpant12.testexecutorplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.bpant12.testexecutorplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
